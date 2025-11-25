e080d9f2db368771fe9d324f23fa2fea63083cbe
tomer <tomzaitsev@gmail.com>
2025-11-25 21:37:21 +0200

    added custom pytorch model class, moved calculations to gpu

--- basic_NN_project.py
+++ basic_NN_project.py
@@ -2,8 +2,13 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder ,StandardScaler
+import torch
+import torch.nn as nn
+import torch.optim as optim
 
-#loading and pre-processing patient data
+#try to use my nvidea 4050
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#loading and pre-processing diamond data
 url = "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/diamonds.csv"
 data = pd.read_csv(url)
 data=data.dropna()
@@ -11,11 +16,57 @@ X=data.drop(columns=['price'])
 Y=data['price']
 categorical_features = X.select_dtypes(exclude=['number']).columns.tolist()
 numerical_features = X.select_dtypes(include=['number']).columns.tolist()
+embedding_sizes = []
 for col in categorical_features:
     le = LabelEncoder()
     X[col] = le.fit_transform(X[col])
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+    num_categories = len(le.classes_)
+    emb_dim = (num_categories + 1) // 2
+    embedding_sizes.append((num_categories, emb_dim))
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 scaler = StandardScaler()
 X_train[numerical_features]=scaler.fit_transform(X_train[numerical_features])
 X_test[numerical_features]=scaler.transform(X_test[numerical_features])
 
+#switch to pytorch tensors
+categorical_train = torch.tensor(X_train[categorical_features].values, dtype=torch.long).to(device)
+numerical_train = torch.tensor(X_train[numerical_features].values, dtype=torch.float).to(device)
+Y_train_tensor = torch.tensor(Y_train.values, dtype=torch.float).reshape(-1, 1).to(device)
+
+
+class DiamondModel(nn.Module):
+    def __init__(self, embedding_sizes, n_cont, layers, p=0.5):
+        super().__init__()
+
+        self.embeddings = nn.ModuleList([
+            nn.Embedding(num_categories, emb_dim)
+            for num_categories, emb_dim in embedding_sizes
+        ])
+        self.n_embs = sum(e.embedding_dim for e in self.embeddings)
+        self.n_cont = n_cont  # This matches len(numerical_features)
+
+        input_size = self.n_embs + self.n_cont
+        layer_list = []
+
+        # create layers
+        for i in layers:
+            layer_list.append(nn.Linear(input_size, i))
+            layer_list.append(nn.ReLU(inplace=True))
+            layer_list.append(nn.BatchNorm1d(i))  # Normalizes data to keep math stable
+            layer_list.append(nn.Dropout(p))  # Randomly disables neurons to prevent overfitting
+            input_size = i  # The output of this layer becomes the input of the next
+
+        # final layer
+        layer_list.append(nn.Linear(layers[-1], 1))
+
+        self.layers = nn.Sequential(*layer_list)
+
+    # logic for passing data through layers
+    def forward(self, x_categorical, x_numerical):
+        embeddings = []
+        for col_idx, emb_layer in enumerate(self.embeddings):
+            embeddings.append(emb_layer(x_categorical[:, col_idx]))
+        x = torch.cat(embeddings, 1)
+        x = torch.cat([x, x_numerical], 1)
+        return self.layers(x)
